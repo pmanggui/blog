f1600bdf028c82cc0f51c4dff382e99c3d0e34e6
pmanggui <ksk2507@naver.com>
2019-03-07 18:59:01 +0900

    *

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '@+rgzykx0jty=+%d5#flq_c0gu$iq0
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
